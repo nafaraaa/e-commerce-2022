e6e866a5b2dd22a614c18291f8b4c4d3398e95f1
Lordsans-404 <entahapa08@gmail.com>
2021-03-22 11:30:12 +0700

    sedikit dihaluskan

--- home/views.py
+++ home/views.py
@@ -2,19 +2,28 @@ from django.shortcuts import render
 from .models import Product,Category
 from django.views.generic import (
 		ListView,
+		DetailView,
 	)
 
 # Create your views here.
 class HomeView(ListView):
 	model = Product
 	category = Category.objects.all()
+	queryset = model.objects.all()
 	extra_context = {
 		'categories':category
 	}
+
+	def get_queryset(self):
+		if len(self.request.GET) != 0:
+			self.queryset = Product.objects.filter(category_id=self.request.GET['category-id'])
+		return super().get_queryset()
+
 	def get_context_data(self):
 		context = super().get_context_data()
 		self.kwargs.update(self.extra_context)
 		kwargs = self.kwargs
 		return context
-	
-		
+
+class ProductHome(DetailView):
+	pass

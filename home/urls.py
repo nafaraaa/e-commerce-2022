74f736690872c4284a7f278a298d85aa2f3944a0
Lordsans-404 <entahapa08@gmail.com>
2021-03-27 21:11:23 +0700

    otw checkout page

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',HomeView.as_view(template_name='home/indexhome.html'),name='index'),
     path('product/',HomeView.as_view(template_name='home/producthome.html'),name='product'),
     path('product/<slug:slug>',ProductHome.as_view(),name='detail'),
-    path('cart/',HomeView.as_view(template_name='home/cart.html'),name="cart")
+    path('cart/',HomeView.as_view(template_name='home/cart.html'),name="cart"),
+    path('checkout/',HomeView.as_view(template_name='home/checkout.html'),name='checkout'),
 ]
--- home/utils.py
+++ home/utils.py
@@ -7,6 +7,9 @@ def cartData(request):
         print(customer)
         order,created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        # items adalah class order yang
+        # mengambil products dari orderitem yang
+        # memiliki hubungan dengan order dari salah satu user
         print(order.get_cart_totals)
     else:
         items = []
